a8835f8997af89207f73c23a62318621df73d260
EdersonHTT <edersondasilvaschmidt@gmail.com>
2025-04-25 21:53:28 -0300

    aaa

--- UC2/Function.php
+++ UC2/Function.php
@@ -40,12 +40,60 @@
 
 /////////////////////////////////////////////////////////////
 
-function vog ($vo){
-    if(preg_match('/[a|e|i|o|u]/', $vo)){
-        return "vogal: sim";
-    } else {
-        return "vogal: no";
-    }
+// function vog ($vo){
+//     if(preg_match('/[a|e|i|o|u]/', $vo)){
+//         return "vogal: sim";
+//     } else {
+//         return "vogal: no";
+//     }
+// }
+
+// echo vog(readline("Uma letra: "));
+
+/////////////////////////////////////////////////////////////////
+
+// function check($idade, $ensino){
+//     if($idade >= 18){
+//         if(strpos($ensino, "s") !== false){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// if(check(readline("idade: "), readline("Tem ensino medio completo: "))){
+//     echo "Pode votar!";
+// } else {
+//     echo "Não pode votar!";
+// }
+
+//////////////////////////////////////////////////////////////////////////////
+
+// function calculoFrete($dis){
+//     if($dis === 0.50){
+//         return $dis * 0.50;
+//     } else if ($dis >= 0.75){
+//         return $dis * 1;
+//     } else {
+//         return "Erro ao calcular o frete";
+//     }
+// }
+
+// echo "frete: " . calculoFrete((float) readline("bote a distancia em km: "));
+
+////////////////////////////////////////////////////////////////////////
+
+function conversor($moeOri, $moeDes, $valor){
+    popen("cls", "w");
+    if ($moeOri === "usd" && $moeDes === "brl"){
+        return "USD -> BRL: " . ($valor * 5.15);
+    } else if ($moeOri === "eur" && $moeDes === "brl"){
+        return "EUR -> BRL: " . ($valor * 5.60);
+    } else if ($moeOri === "usd" && $moeDes === "eur"){
+        return "USD -> EUR: " . ($valor * 1.45);
+    } 
+    return "Erro no convertor!";
 }
 
-echo vog(readline("Uma letra: "));
+echo "Escolha o que converter:\n\nUSD -> BRL\nEUR -> BRL\nUSD -> EUR\n\n";
+echo conversor(strtolower(readline("Escreva a moeda que ira converter: ")), strtolower(readline("Escreva a moeda de destino: ")), (float) readline("Escreva a moeda que ira converter: "));
