0305a45329944f4dec82c41099612cb4f3e47613
EdersonHTT <edersondasilvaschmidt@gmail.com>
2025-05-27 17:19:35 -0300

    feat(color) cor adicionada ao mapa

--- game/game.js
+++ game/game.js
@@ -1,23 +1,36 @@
 let ask = require("readline-sync")
 
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
 function seeMap(map){
 
-    for(i of map){
-        if(map.indexOf(i) === 0){
-            console.log("______________________________")
-            console.log(`|${i.join("  ")}|`)
-        } else if(map.indexOf(i) === map.length-1){
-            console.log(`|${i.join("  ")}|`)
-            console.log("|____________________________|")
-        } else {
-            console.log(`|${i.join("  ")}|`)
+    let copy = [[], [], [], [], [], [], [], [], [], []]
+
+    for(let i = 0; i < copy.length; i++){
+
+        for(let k = 0; k < map[i].length; k++){
+
+            copy[i].push(`\x1b[42m${map[i][k]}\x1b[0m`)
         }
 
-        
+        if(copy.indexOf(copy[i]) === 0){
+
+            console.log("______________________________")
+            console.log(`|${copy[i].join("\x1b[42m  \x1b[0m")}|`)
+        } else if(copy.indexOf(copy[i]) === copy.length-1){
+
+            console.log(`|${copy[i].join("\x1b[42m  \x1b[0m")}|`)
+            console.log("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
+        } else {
+
+            console.log(`|${copy[i].join("\x1b[42m  \x1b[0m")}|`)
+        }
     }
 }
 
-function move(map, direction){
+function move(map, direction, forms){
     let horizontal = 0
     let vertical = 0
 
@@ -35,45 +48,67 @@ function move(map, direction){
 
         for(let k = 0; k<map[i].length; k++){
 
-            if(map[i][k] === "P"){
-                if(map.includes(map[(i+vertical)]) && map[(i+vertical)].includes(map[(i+vertical)][(k + horizontal)]) && map[(i + vertical)][(k + horizontal)] != "T" && map[(i+vertical)][(k+horizontal)] != "M" ){
-                    ask.question(map[(i)][(k)])
-                    ask.question(map[(i+vertical)][(k+horizontal)])
+            if(map[i][k] === forms[2] && map.includes(map[(i+vertical)]) && map[(i+vertical)].includes(map[(i+vertical)][(k + horizontal)])){
+
+                if(map[(i + vertical)][(k + horizontal)] != forms[0] && map[(i+vertical)][(k+horizontal)] != forms[1] && map[(i+vertical)][(k+horizontal)] != forms[3]){
+                    
                     let temp = map[(i+vertical)][(k+horizontal)]
                     map[(i+vertical)][(k+horizontal)] = map[i][k]
                     map[i][k] = temp
-                    break
+                    return
+                } else if(map[(i+vertical)][(k+horizontal)] === forms[1]){
+
+                    map[(i+vertical)][(k+horizontal)] = map[i][k]
+                    map[i][k] = " "
                 }
             }
         }
     }
 }
 
-let map = [
-    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", "T", "T", " ", " ", " ", "M", " ", "T", " "],
-    [" ", "T", " ", " ", " ", " ", " ", "T", "T", " "],
-    [" ", "T", " ", " ", " ", " ", " ", " ", "T", " "],
-    [" ", " ", " ", " ", "T", "T", " ", " ", " ", " "],
-    ["S", " ", " ", " ", " ", "T", " ", " ", " ", " "],
-    ["S", " ", " ", " ", " ", "T", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", "T", " ", " ", " ", " ", " ", " ", " ", " "],
-    ["T", "T", " ", " ", " ", " ", " ", " ", " ", "T"],
-    ["M", " ", " ", " ", " ", " ", " ", "P", "T", "T"],
-]
-
-let conut = 0
-
-while(true){
-
+function start(){    
 
     console.clear()
     seeMap(map)
-    move(map, ask.question("move: "))
+    process.stdin.on('data', (tecla) => {
+        
+        if(tecla != "\u001B"){
+            
+            move(map, tecla, [tree, monster, formP, item])
+            console.clear()
 
-    conut++
-    if(conut === 999){
-        break
-    }
+            seeMap(map)
+        } else {
+
+            process.exit()
+        }
+            
+    })
 }
+
+let tree = "\x1b[92m▲\x1b[0m"
+let monster = "\x1b[31m■\x1b[0m"
+let item = "\x1b[34m°\x1b[0m"
+let formP = "\x1b[37m●\x1b[0m"
+
+let map = [
+    [monster, tree, tree, " ", " ", " ", monster, " ", tree, item],
+    [" ", tree, " ", " ", " ", " ", " ", tree, tree, " "],
+    [" ", tree, " ", " ", " ", " ", " ", " ", tree, " "],
+    [" ", " ", " ", " ", tree, tree, " ", " ", " ", " "],
+    [" ", " ", " ", " ", item, tree, " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", tree, " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", tree, " ", " ", " ", " ", " ", " ", " ", " "],
+    [tree, tree, " ", " ", " ", " ", " ", " ", " ", tree],
+    [monster, " ", " ", " ", " ", " ", " ", formP, tree, tree],
+]
+
+let player = {
+
+    nome: "",
+    hp: 100,
+    inventory: []
+}
+
+start()
