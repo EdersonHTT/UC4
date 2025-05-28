35dcfd2342bbb860849740ef013031687cb04505
EdersonHTT <edersondasilvaschmidt@gmail.com>
2025-05-27 21:37:24 -0300

    aaaaaaaa

--- game/game.js
+++ game/game.js
@@ -28,7 +28,7 @@ function seeMap(map){
     }
 }
 
-function move(map, direction, forms){
+function move(map, direction, forms, player, itens){
     let horizontal = 0
     let vertical = 0
 
@@ -56,9 +56,36 @@ function move(map, direction, forms){
                     return
                 } else if(map[(i+vertical)][(k+horizontal)] === forms[1]){
 
+
                     map[(i+vertical)][(k+horizontal)] = map[i][k]
                     map[i][k] = " "
-                }
+                    return
+                } else if(map[(i+vertical)][(k+horizontal)] === forms[3]){
+                    
+                    if(map.indexOf(map[i]) === 0, map[i].indexOf(map[i][k]) === 9){
+
+                        player.inventory.push(itens[0])
+                        map[(i+vertical)][(k+horizontal)] = map[i][k]
+                        map[i][k] = " "
+                    } else if(map.indexOf(map[i]) === 3, map[i].indexOf(map[i][k]) === 4){
+
+                        player.inventory.push({...itens[1]})
+                        
+                        for(let v = 0; v < player.inventory.length; v++){
+
+                            if(player.inventory[v].potion === "life"){
+
+                                player.inventory[v].amount += 3
+                            }
+                        }
+                        map[(i+vertical)][(k+horizontal)] = map[i][k]
+                        map[i][k] = " "
+                        return
+                    }
+                    
+                    
+                    return
+                } 
             }
         }
     }
@@ -72,10 +99,10 @@ function start(){
         
         if(tecla != "\u001B"){
             
-            move(map, tecla, [tree, monster, formP, item])
+            move(map, tecla, [tree, monster, formP, item], player, itens)
             console.clear()
-
             seeMap(map)
+            console.log(player.inventory)
         } else {
 
             process.exit()
@@ -91,7 +118,7 @@ let formP = "\x1b[37m●\x1b[0m"
 
 let map = [
     [monster, tree, tree, " ", " ", " ", monster, " ", tree, item],
-    [" ", tree, " ", " ", " ", " ", " ", tree, tree, " "],
+    [" ", tree, " ", " ", " ", " ", " ", tree, tree, monster],
     [" ", tree, " ", " ", " ", " ", " ", " ", tree, " "],
     [" ", " ", " ", " ", tree, tree, " ", " ", " ", " "],
     [" ", " ", " ", " ", item, tree, " ", " ", " ", " "],
@@ -106,7 +133,56 @@ let player = {
 
     nome: "",
     hp: 100,
+    damage: 5,
     inventory: []
 }
+
+let itens = [
+
+    {
+
+        arma: "sword",
+        dano: 10
+    },
+
+    {
+
+        potion: "life",
+        heal: 10,
+        amount: 0
+    },
+
+    {
+
+        potion: "strength",
+        buff: 15,
+        shifts: 3,
+        amount: 0
+    },
+
+    {
+
+        armor: "helmet",
+        hp: 5,
+    },
+
+    {
+
+        armor: "chestplate",
+        hp: 10,
+    },
+
+    {
+
+        armor: "pants",
+        hp: 10,
+    },
+
+    {
+
+        armor: "boots",
+        hp: 5,
+    }
+]
 
 start()
