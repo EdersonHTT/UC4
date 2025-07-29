aef0c704d5de61d4c9006a0236ab28c7893e453c
EdersonSS <166855651+EdersonHTT@users.noreply.github.com>
2025-07-29 20:50:40 -0300

    Add files via upload

--- UC3/Aula-8-back/server.js
+++ UC3/Aula-8-back/server.js
@@ -21,13 +21,42 @@ connection.connect(error => {
     console.log('Conectado ao banco de dados com ID ' + connection.threadId);
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.post('/usuarios', (req, res) => {
+    const { nome, email, senha } = req.body;
+    const sql = 'INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)';
+    connection.query(sql, [nome, email, senha], (error) => {
+        if (error) return res.status(500).send('Erro ao adicionar usuário.');
+        res.status(201).send('Usuário adicionado com sucesso.');
+    });
 });
 
-app.post('/usuarios', (req, res) => {
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const senha = req.body.senha;
-  });
+app.get('/usuarios', (req, res) => {
+    const sql = "SELECT * FROM usuarios;";
+    connection.query(sql, (error, info) => {
+        if (error) return res.status(500).send('Erro ao mostrar usuário.');
+        res.status(200).send(info);
+    });
+});
+
+app.put('/usuarios', (req, res) => {
+    const { id, dado, info } = req.body;
+    const sql = `UPDATE usuarios SET ${dado} = ? WHERE id = ?;`;
+    connection.query(sql, [info, id], (error) => {
+        if (error) return res.status(500).send('Erro ao atualizar usuário.');
+        res.status(201).send('Usuário atualizado com sucesso.');
+    });
+});
+
+app.delete('/usuarios', (req, res) => {
+    const { id } = req.body;
+    const sql = "DELETE FROM usuarios WHERE id = ?;";
+    connection.query(sql, [ id ], (error, info) => {
+        if (error) return res.status(500).send('Erro ao deletar usuário.');
+        res.status(200).send("Usuario deletado com sucesso");
+    });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
