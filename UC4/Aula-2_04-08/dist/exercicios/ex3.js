04f558abb7f425499b385426a71989f90a4f6d7f
EdersonSS <166855651+EdersonHTT@users.noreply.github.com>
2025-08-04 20:51:56 -0300

    Add files via upload

---
+++ UC4/Aula-2_04-08/dist/exercicios/ex3.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.saudação = saudação;
+function saudação(aluno) {
+    console.log(`Olá, ${aluno}! Bem-vindo(a) à turma.`);
+}
