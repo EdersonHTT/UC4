96d4d9b0bd3c7847e1e346fe5d7ce22e135c2b09
EdersonHTT <edersondasilvaschmidt@gmail.com>
2025-09-05 20:21:06 -0300

    aa

--- TestPOO/test.java
+++ TestPOO/test.java
@@ -23,5 +23,13 @@ Questões de Interpretação de Código
 1 - Vai dar erro por conta que esta chamando o nome que é private em vez de chamar o geter de nome.
 
 2 - 
+Woof!
+This dog is a Golden Retriever breed.
+Fetching...
 
+3 - 
+vai dar erro por conta que square não extende Shape
+
+4 - 
+Sim, ocorrerá erro por conta que não pode por logica.
  */
