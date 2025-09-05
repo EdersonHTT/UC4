386e19eb7194234f2e08eea13db050918ebf7dde
EdersonHTT <edersondasilvaschmidt@gmail.com>
2025-09-04 21:49:37 -0300

    test

---
+++ TestPOO/test.java
@@ -0,0 +1,27 @@
+/*
+
+1 - D
+
+2 - C
+
+3 - B
+
+4 - B
+
+5 - A
+
+6 - A
+
+7 - D
+
+8 - B
+
+9 - C
+
+Questões de Interpretação de Código
+
+1 - Vai dar erro por conta que esta chamando o nome que é private em vez de chamar o geter de nome.
+
+2 - 
+
+ */
